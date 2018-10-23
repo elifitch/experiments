5fc9a8d83301ed46546f55700f7d5444b070c0dd
Eli Fitch <eli.fitch.prof@gmail.com>
2018-10-23 07:00:33 -0400

    scalable pattern

--- src/projects/model-grid-unlit/src/js/index.js
+++ src/projects/model-grid-unlit/src/js/index.js
@@ -8,16 +8,21 @@ import ImportModel from './import-model';
 import 'three/OrbitControls';
 import distributePattern from './distribute-pattern';
 
+const PATTERN_SCALE = 500;
+const TEXTURE_RESOLUTION = 1024;
+
 const containerEl = document.getElementsByClassName('container')[0];
 let cW = containerEl.offsetWidth;
 let cH = containerEl.offsetHeight;
-let wW = window.innerWidth;
-let wH = window.innerHeight;
+const initialWidth = containerEl.offsetWidth;
+const initialHeight = containerEl.offsetHeight;
+
+let bgPlane;
 
 // TODO: need to figure a way to dynamically set the texture size
 // based on the window size
 let rtTexture = new THREE.WebGLRenderTarget(
-  1024, 1024,
+  TEXTURE_RESOLUTION, TEXTURE_RESOLUTION,
   {
     minFilter: THREE.LinearFilter,
     magFilter: THREE.NearestFilter,
@@ -26,7 +31,7 @@ let rtTexture = new THREE.WebGLRenderTarget(
 );
 rtTexture.texture.wrapS = THREE.RepeatWrapping;
 rtTexture.texture.wrapT = THREE.RepeatWrapping;
-rtTexture.texture.repeat.set(8, 4.5);
+rtTexture.texture.repeat.set(cW / PATTERN_SCALE, cH / PATTERN_SCALE);
 
 const renderer = Renderer({ containerEl, clearColor: 0xffffff });
 const verticalOffset = -0.15;
@@ -44,7 +49,7 @@ const { scene: displayScene, camera: displayCamera } = Scene({
     1, 1000
   ),
   cameraPos: [0, 0, 18],
-  cameraAspect: window.innerWidth / window.innerHeight,
+  cameraAspect: cW / cH,
   cameraFov: 20
 });
 
@@ -52,25 +57,36 @@ const controls = new THREE.OrbitControls(rttCamera);
 controls.enableDamping = true;
 controls.rotateSpeed = 0.5;
 controls.dampingFactor = 0.25;
+controls.enableZoom = false;
 controls.target = new THREE.Vector3(0, verticalOffset, 0);
 
 ImportModel().then(meshGroup => {
   rttScene.add(distributePattern(meshGroup));
 
   // ----------------------------
-  const plane = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(window.innerWidth, window.innerHeight),
+  bgPlane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(cW, cH),
     new THREE.MeshBasicMaterial({ map: rtTexture.texture })
   );
-  displayScene.add(plane);
+  displayScene.add(bgPlane);
 });
 
 window.addEventListener('resize', () => {
-  let cW = containerEl.offsetWidth;
-  let cH = containerEl.offsetHeight;
+  cW = containerEl.offsetWidth;
+  cH = containerEl.offsetHeight;
+  const scaleFactorW = cW / initialWidth;
+  const scaleFactorH = cH / initialHeight;
+
+  bgPlane.scale.set(scaleFactorW, scaleFactorH, 1);
+  rtTexture.texture.repeat.set(cW / PATTERN_SCALE, cH / PATTERN_SCALE);
+
   renderer.setSize(cW, cH);
-  rttCamera.aspect = cW / cH;
-  rttCamera.updateProjectionMatrix();
+
+  displayCamera.left = cW / -2;
+  displayCamera.right = cW / 2;
+  displayCamera.top = cH / 2;
+  displayCamera.bottom = cH / -2;
+  displayCamera.updateProjectionMatrix();
 });
 
 RenderLoop({ renderer, rtTexture, scenes: [rttScene, displayScene], cameras: [rttCamera, displayCamera], controls });
